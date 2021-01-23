4dd925b0b8456f4488671d1e1749697b9923c28a
Meltem Akdenizli <meltem1995.ma@gmail.com>
2021-01-24 01:28:05 +0300

    20 Ocak

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kampİntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
+class Kurs
+{
+	public string KursAdi { get; set; }
+	public string Egitmeni { get; set; }
+	public string İzlenmeOrani { get; set; }
+}
