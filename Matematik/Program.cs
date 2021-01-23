a9cb047a4bd7bd06a5697a3745910dc8c3d774cc
Meltem Akdenizli <meltem1995.ma@gmail.com>
2021-01-24 00:55:35 +0300

    20 Ocak Ders Sonu

---
+++ Matematik/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Matematik
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DortIslem mat = new DortIslem();
+			mat.Topla(5,6);
+
+			mat.Topla(7,8);
+		}
+	}
+}
+
