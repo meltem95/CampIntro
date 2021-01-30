d8883212a3fdcae5cffe97d668365d18f290cd10
Meltem Akdenizli <meltem1995.ma@gmail.com>
2021-01-31 01:59:54 +0300

    27 Ocak

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager:IUserValidationService
+    {
+        public void Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1985 && gamer.FirstName == "Engin"
+                && gamer.LastName == "Demiroğ" && gamer.İdentityNumber == 12345)
+            {
+                return true; 
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ SQL.sql
@@ -0,0 +1,33 @@
+﻿--Select
+Select ContactName Adi, CompanyName SirketAdi, City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+-- case insensitive
+Select* from Products where categoryId =1 or CategoryId =3
+
+Select* from Products where categoryId =1 and UnitPrice>=10
+
+
+select * from Products where CategoryID=1 order by UnitPrice desc -- ascending -- descending
+
+Select count(*) Adet from Products 
+
+Select CategoryID,count(*) from Products where UnitPrice>20 group by CategoryID having count(*)<10
+
+Select Products.ProductID, Products.ProductName,Products.UnitPrice,Categories.CategoryName 
+from Products inner join Categories
+on Products.CategoryID= Categories.CategoryID
+where Products.UnitPrice>10
+
+--DTO Data transformation object
+
+Select * from Products p inner join [Order Details] od
+on p.ProductID = od.ProductID 
+inner join Orders o
+on o.OrderID = od.OrderID
+
+select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID
+where o.CustomerID is null
+
